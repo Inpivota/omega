dc9395ddff74c3827294d8442da8950545fb352f
Tyler <tylerwillfixit@gmail.com>
2019-01-23 14:20:15 +0000

    made the table more responsive

--- ui/src/views/ForecastingView.jsx
+++ ui/src/views/ForecastingView.jsx
@@ -9,44 +9,50 @@ import TableCell from "@material-ui/core/TableCell/TableCell";
 import TableBody from "@material-ui/core/TableBody/TableBody";
 import Paper from "@material-ui/core/Paper/Paper";
 import {Area, AreaChart} from "recharts";
+import TextField from "@material-ui/core/TextField/TextField";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 700,
+    },
+});
 
 const ForecastingView = (props) => {
+    const {classes} = props;
     const graph = (data) => {
         return (
-            <AreaChart width={200} height={60} data={data}
-                       margin={{top: 5, right: 0, left: 0, bottom: 5}}>
+            <AreaChart width={200} height={60} data={data}>
                 <Area type='monotone' dataKey='sales' stroke='#8884d8' fill='#8884d8' />
             </AreaChart>
         );
-        // return (
-        //     <AreaChart width={200} height={60} data={data}>
-        //         <Area type='monotone' dataKey='value' stroke='#8884d8' fill='#8884d8' />
-        //     </AreaChart>
-        // )
     };
     const salesDataObjects = (sales) => sales.map(value => {return {sales:value}});
     const data = [
         {
             name:"Vitamin C (1 lb) POI",
             salesHistory: salesDataObjects([5,10,14,17,12,8,7,3,14,16,20,26,27,30,34,31,28,25,23,26,29,20,15,21,16,10,18,6,2,1,1]),
-            predictedAvg: 19.03
+            predictedAvg: 19.03,
+            manualAverage: 34,
+            amazonInventory: 625,
+            shipped: 630,
         }
     ];
     return (
-        <Paper>
-            <Grid container>
-                <AreaChart width={200} height={60} data={data.salesHistory}
-                           margin={{top: 5, right: 0, left: 0, bottom: 5}}>
-                    <Area type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
-                </AreaChart>
-                <Table>
+        <Paper className={classes.root}>
+                <Table className={classes.table}>
                     <TableHead>
                         <TableRow>
                             <TableCell>Name</TableCell> {/*Flag icon will be present right next to the name*/}
                             <TableCell>Sales History</TableCell>
                             <TableCell>Predicted Average</TableCell>
                             <TableCell>Manual Average</TableCell>
-                            <TableCell>FBA Inventory</TableCell>
+                            <TableCell>Amazon Inventory</TableCell>
                             <TableCell>Shipped</TableCell>
                             <TableCell>Actions</TableCell>
                         </TableRow>
@@ -57,13 +63,20 @@ const ForecastingView = (props) => {
                                 <TableCell>{item.name}</TableCell>
                                 <TableCell>{graph(item.salesHistory)}</TableCell>
                                 <TableCell>{item.predictedAvg}</TableCell>
+                                <TableCell>
+                                    <TextField
+                                        value={item.manualAverage}
+                                        // onChange={updateItem}
+                                    />
+                                </TableCell>
+                                <TableCell>{item.amazonInventory}</TableCell>
+                                <TableCell>{item.shipped}</TableCell>
                             </TableRow>)
                         )}
                     </TableBody>
                 </Table>
-            </Grid>
         </Paper>
     )
 };
 
-export default ForecastingView;
+export default withStyles(styles)(ForecastingView);
