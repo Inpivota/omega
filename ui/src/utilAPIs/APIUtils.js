f5ceb48c683067a6edf974e5c495860f3f07c175
Tyler <tylerwillfixit@gmail.com>
2019-03-19 04:33:53 +0000

    added graphql request to the forecast view

--- ui/src/utilAPIs/APIUtils.js
+++ ui/src/utilAPIs/APIUtils.js
@@ -1,5 +1,25 @@
-
 export const handleErrors = (response) => {
-    if(response.ok) return response.json();
+    if (response.ok) return response.json();
     else throw Error(response.statusText)
+};
+
+export const getForcastData = () => {
+    const query = `query {
+        productsForForecasting {
+            description,
+            finaleId,
+            getBuildOutToDays,
+            name,
+            getProductAmazonInventory,
+            getProductShippedInventory,
+        }
+    }`;
+    return fetch("/graphql", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({query}),
+    }).then(response => handleErrors(response))
+    
 };
